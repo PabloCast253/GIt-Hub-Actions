103a78f2ee37361dd2060490e937ab9d62cf5481
unknown <pablocast253@gmail.com>
2025-03-31 19:54:06 -0700

    changed some seeds files

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,14 +3,19 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+    const model = models[modelName];
+    if (!model) {
+      throw new Error(`Model ${modelName} not found.`);
+    }
 
-    if (modelExists.length) {
+    const collections = await model.db?.db
+      ?.listCollections({ name: collectionName })
+      ?.toArray() || [];
+
+    if (collections.length) {
       await db.dropCollection(collectionName);
     }
   } catch (err) {
     throw err;
   }
-}
+};
